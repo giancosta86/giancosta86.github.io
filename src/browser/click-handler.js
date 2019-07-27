9346d0a8b66ced0368298ef40e17c2102e3ed1fa
Gianluca Costa <gianluca@gianlucacosta.info>
2019-07-27 20:06:14 +0200

    Remove useless logging

--- src/browser/click-handler.js
+++ src/browser/click-handler.js
@@ -4,8 +4,6 @@ document.addEventListener("click", event => {
   let clickedElement = event.target
 
   if (clickedElement.href && BlankTargetRegex.test(clickedElement.href)) {
-    console.log(JSON.stringify(clickedElement.href))
-
     clickedElement.target = "_blank"
     clickedElement.rel = "noopener"
   }
