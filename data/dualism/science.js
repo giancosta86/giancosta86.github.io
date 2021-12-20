073e9d34cf5e496ed27dd04a2fd3217bfc73da09
Gianluca Costa <gianluca@gianlucacosta.info>
2021-12-20 06:47:56 +0100

    Update Science area

--- data/dualism/science.js
+++ data/dualism/science.js
@@ -3,13 +3,13 @@ export let scienceMarkdown = `
 
 * ⌨️ Passionate **software craftsman** *since 1999*
 
-* 💼 More than **7 years** of experience in the *IT industry*
+* 💼 More than **7 years** of experience in *IT companies*
 
 * 💻 **MSc** and **BSc** in **Computer Engineering** - *110 cum laude*
 
 * 🎓 One of the **30 best Engineering students** in Bologna, in 2016
 
-* 🤖 More than **80 open source projects** on [GitHub](https://github.com/giancosta86)
+* 🤖 More than **70 open source projects** on [GitHub](https://github.com/giancosta86)
 
 * 🐧 **Linux** user *since 2004* - mainly on Debian-based distros
 
