552b0d451eddad0ca86ead89f4bcb8d412b3de46
Gianluca Costa <gianluca@gianlucacosta.info>
2022-05-01 04:02:26 +0200

    Move the CSS module next to its page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import Link from "next/link";
-import styles from "../styles/Home.module.scss";
+import styles from "./index.module.scss";
 
 import ReactMarkdown from "react-markdown";
 import { scienceMarkdown } from "../data/dualism/science";
